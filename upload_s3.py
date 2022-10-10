b50a4a60516b76b147a75b0b3b4c1763fc819149
vijay bhaskar <vijay.bhaskar@elementure.tech>
2022-10-10 15:44:50 +0530

    image loading

--- upload_s3.py
+++ upload_s3.py
@@ -5,3 +5,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 s3_client = boto3.s3_client('s3',region_name = os.getenv('REGION_NAME'),aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+s3.meta.client.upload_file('./vijay.png', 'profilepictureobject')
