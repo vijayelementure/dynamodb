4096ee102e6370e7a38fa7bb9470860aa27d2d68
vijay bhaskar <vijay.bhaskar@elementure.tech>
2022-10-11 13:08:04 +0530

    s3 related

--- download_s3.py
+++ download_s3.py
@@ -5,4 +5,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 s3 = boto3.resource('s3',region_name = os.getenv('REGION_NAME'),aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-s3.meta.client.download_file('profilepictureobject', 'god.png', './god.jpeg')
+response = s3.meta.client.download_file('profilepictureobject', 'god.png', './god.jpeg')
+
+print(response)
---
+++ gets3object.py
@@ -0,0 +1,7 @@
+import boto3
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+s3 = boto3.client('s3',region_name=os.getenv('REGION_NAME'),aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
