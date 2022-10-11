8b64cfa1409eb68993721f1ba32cae18500cd976
vijay bhaskar <vijay.bhaskar@elementure.tech>
2022-10-11 11:22:33 +0530

    user profile

--- user_profile.py
+++ user_profile.py
@@ -8,19 +8,14 @@ from datetime import datetime
 load_dotenv()
 
 dynamodb = boto3.resource('dynamodb',region_name = os.getenv('REGION_NAME'),aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
-table = dynamodb.Table('fueblockapp')   
-
-
-
+table = dynamodb.Table('userprofile') 
 
 table.put_item(
     Item={
     "email":"vijaybhaskarmyv@gmail.com",
-    "FullName":"",
-    "mobile":"",
-    "HashedPassword":"",
-    "uuid":"",
-    "account created": str(datetime.now())
+    "profile photo":"",
+    "uuid":0,
+    "Created Date": str(datetime.now())
     }
 
 )
