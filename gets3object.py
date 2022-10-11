1d6cd342f2857e665cfa9273add210225f322610
vijay bhaskar <vijay.bhaskar@elementure.tech>
2022-10-11 13:59:12 +0530

    modified

--- gets3object.py
+++ gets3object.py
@@ -5,3 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 s3 = boto3.client('s3',region_name=os.getenv('REGION_NAME'),aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
+response = s3.get_bucket_location(
+    Bucket='profilepictureobject',
+    # ExpectedBucketOwner='string'
+)['LocationConstraint']
+
+print(response)
+
