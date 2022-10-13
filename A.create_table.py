21a261da604f2a5bd886ae56c229cbab90e182fb
vijay bhaskar <vijay.bhaskar@elementure.tech>
2022-10-13 18:32:49 +0530

    Initial commit

--- A.create_table.py
+++ A.create_table.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 dynamodb = boto3.resource('dynamodb',region_name=os.getenv('REGION_NAME'),aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'))
 table = dynamodb.create_table(
-    TableName='userprofile',
+    TableName='lock_data',
     KeySchema=[
         {
             'AttributeName': 'email',
